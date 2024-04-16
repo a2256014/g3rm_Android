7f461f985ee622a155e451e418f8f874ad30756d
g3rm <a2256014@gmail.com>
2024-04-16 22:23:06 +0900

    login1

---
+++ app/src/main/java/com/example/g3rm_ah/common/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.g3rm_ah.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/g3rm_ah/common/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.example.g3rm_ah.common;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+}
